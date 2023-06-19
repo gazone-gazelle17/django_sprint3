64d925402fb65abfff7cbbd45b16c5cab05939cb
Александр Гасымов <gazykh@yandex.ru>
2023-06-20 01:47:26 +0300

    Отправка на первое ревью

---
+++ blogicum/blog/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import get_object_or_404, render
+from datetime import datetime
+from .models import Post, Category
+
+
+def index(request):
+    post_list = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+        ).order_by('-pub_date')[:5]
+    context = {'post_list': post_list}
+    return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post.objects.filter(
+        pk=post_id,
+        pub_date__lte=datetime.now(),
+        is_published=True,
+        category__is_published=True
+        )
+    )
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
+
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
+    post_list = Post.objects.filter(
+        category=category,
+        pub_date__lte=datetime.now(),
+        is_published=True
+    ).order_by('-pub_date')
+
+    context = {
+        'post_list': post_list,
+        'category_slug': category_slug,
+        'category': category
+    }
+
+    return render(request, 'blog/category.html', context)
