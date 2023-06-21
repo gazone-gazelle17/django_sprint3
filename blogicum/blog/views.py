d7db226e7ef752119d9099020f4c62bac296f793
Александр Гасымов <gazykh@yandex.ru>
2023-06-21 23:27:04 +0300

    Устранение замечаний после первого ревью

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from datetime import datetime
+from django.utils import timezone
+
 from .models import Post, Category
 
 
@@ -7,19 +8,19 @@ def index(request):
     post_list = Post.objects.filter(
         is_published=True,
         category__is_published=True,
-        pub_date__lte=datetime.now()
+        pub_date__lte=timezone.now()
     ).order_by('-pub_date')[:5]
     context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post.objects.filter(
+    post = get_object_or_404(
+        Post,
         pk=post_id,
-        pub_date__lte=datetime.now(),
+        pub_date__lte=timezone.now(),
         is_published=True,
         category__is_published=True
-    )
     )
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
@@ -32,14 +33,12 @@ def category_posts(request, category_slug):
         is_published=True)
     post_list = Post.objects.filter(
         category=category,
-        pub_date__lte=datetime.now(),
+        pub_date__lte=timezone.now(),
         is_published=True
     ).order_by('-pub_date')
-
     context = {
         'post_list': post_list,
         'category_slug': category_slug,
         'category': category
     }
-
     return render(request, 'blog/category.html', context)
